6fd077dad8cda3171e3b11a00f39e633faa86c84
Jarvis <jarvisshim@gmail.com>
2026-02-23 14:53:19 +0900

    sitemap 통합 옵션 추가 - 하나 파일로 통합

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://aisnowball.work',
+  sitemap: {
+    site: 'https://aisnowball.work',
+    compress: true,
+  },
   integrations: [sitemap()],
 });
